c464d06692c668fa155770ffe312c5aef404770b
LeeYG03 <LeeYG3673@gmail.com>
2025-08-28 20:25:21 +0900

    커밋11

---
+++ src/main/java/com/example/funding/controller/MyPageController.java
@@ -0,0 +1,11 @@
+package com.example.funding.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/user")
+public class MyPageController {
+    //MyPageService service;
+
+    //@GetMapping
+}

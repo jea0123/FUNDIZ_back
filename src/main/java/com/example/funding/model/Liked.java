ca51e89bbe5d1835af8ed627fcdb5470e75b0828
hyukx2 <carrot7527@gmail.com>
2025-08-28 19:23:14 +0900

    test

---
+++ src/main/java/com/example/funding/model/Liked.java
@@ -0,0 +1,18 @@
+package com.example.funding.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@Builder
+public class Liked {
+    private long userId;
+    private long projectId;
+    private Date createdAt;
+}

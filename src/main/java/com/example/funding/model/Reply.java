c00d9af39c8eb53382de40f323c2824d52f60e74
hyukx2 <carrot7527@gmail.com>
2025-08-28 18:30:48 +0900

    test

---
+++ src/main/java/com/example/funding/model/Reply.java
@@ -0,0 +1,22 @@
+package com.example.funding.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@Builder
+public class Reply {
+    private long replyId;
+    private long userId;
+    private String content;
+    private char isSecret;
+    private Date createdAt;
+    private Date deletedAt;
+    private char code;
+}

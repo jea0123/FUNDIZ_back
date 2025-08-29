717e04a74c4e56a723166b102b8e161494a83f4e
jea0123 <eunae8895@gmail.com>
2025-08-29 19:01:51 +0900

    pull commit

---
+++ src/main/java/com/example/funding/mapper/CommunityMapper.java
@@ -0,0 +1,12 @@
+package com.example.funding.mapper;
+
+import com.example.funding.model.Community;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+@Mapper
+public interface CommunityMapper {
+    List<Community> getCommunityListById(@Param("projectId") Long projectId);
+}

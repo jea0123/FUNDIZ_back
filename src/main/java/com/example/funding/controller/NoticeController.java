c5a4a129ff0cc48b86422c2c70ec98f29dec8c9c
KRMGJ <dur369667@naver.com>
2025-09-02 21:42:36 +0900

    fix notice

--- src/main/java/com/example/funding/controller/NoticeController.java
+++ src/main/java/com/example/funding/controller/NoticeController.java
@@ -6,7 +6,6 @@ import com.example.funding.service.NoticeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -18,28 +17,28 @@ public class NoticeController {
 
     private final NoticeService noticeService;
 
-        @GetMapping
-        ResponseEntity<ResponseDto<List<Notice>> list() {
-            return noticeService.list();
-        }
+    @GetMapping
+    ResponseEntity<ResponseDto<List<Notice>>> list() {
+        return noticeService.list();
+    }
 
-        @GetMapping("/{noticeId}")
-        public ResponseEntity<ResponseDto<Notice>> getNoticeDetail(@PathVariable("noticeId") Long noticeId) {
-            return noticeService.getNoticeDetail(noticeId);
-        }
+    @GetMapping("/{noticeId}")
+    public ResponseEntity<ResponseDto<Notice>> getNoticeDetail(@PathVariable("noticeId") Long noticeId) {
+        return noticeService.getNoticeDetail(noticeId);
+    }
 
-        @PostMapping
-        public ResponseEntity<ResponseDto<Notice>> add(@RequestBody Notice item) {
-            return noticeService.add(item);
-        }
+    @PostMapping
+    public ResponseEntity<ResponseDto<Notice>> add(@RequestBody Notice item) {
+        return noticeService.add(item);
+    }
 
-        @PutMapping
-        public ResponseEntity<ResponseDto<Notice>> update(@RequestBody Notice item) {
-            return noticeService.update(item);
-        }
+    @PutMapping
+    public ResponseEntity<ResponseDto<Notice>> update(@RequestBody Notice item) {
+        return noticeService.update(item);
+    }
 
-        @DeleteMapping("/{noticeId}")
-        void delete(@PathVariable Long noticeId) {
-            noticeService.delete(noticeId);
+    @DeleteMapping("/{noticeId}")
+    void delete(@PathVariable Long noticeId) {
+        noticeService.delete(noticeId);
     }
 }

d2ca1039ebceb015346762cf1ea9a52590c080d1
LeeYG03 <LeeYG3673@gmail.com>
2025-09-04 21:18:11 +0900

    25/09/04 21:00까지의 작업본(미완성)

--- src/main/java/com/example/funding/mapper/backingDetailMapper.java
+++ src/main/java/com/example/funding/mapper/backingDetailMapper.java
@@ -1,5 +1,6 @@
 package com.example.funding.mapper;
 
+import com.example.funding.dto.response.user.BackingProjectDto;
 import com.example.funding.model.BackingDetail;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
@@ -9,4 +10,8 @@ import java.util.List;
 @Mapper
 public interface BackingDetailMapper {
     List<BackingDetail> findByBackingIds(@Param("ids") List<Long> backingIds);
+
+    //후원한 프로젝트 상세정보
+    BackingProjectDto getBackingProjectAndUserId(@Param("projectId")Long projectId, @Param("userId") Long userId);
+
 }

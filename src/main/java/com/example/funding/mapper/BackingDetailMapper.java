e3183b88cb409cdf5cb4be0d1cf197a12e274719
LeeYG03 <LeeYG3673@gmail.com>
2025-09-04 21:33:13 +0900

    refact

---
+++ src/main/java/com/example/funding/mapper/BackingDetailMapper.java
@@ -0,0 +1,16 @@
+package com.example.funding.mapper;
+
+import com.example.funding.dto.response.user.BackingProjectDto;
+import com.example.funding.model.BackingDetail;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+@Mapper
+public interface BackingDetailMapper {
+    List<BackingDetail> findByBackingIds(@Param("ids") List<Long> backingIds);
+
+    //후원한 프로젝트 상세정보
+    BackingProjectDto getBackingProjectAndUserId(@Param("projectId")Long projectId, @Param("userId") Long userId);
+}

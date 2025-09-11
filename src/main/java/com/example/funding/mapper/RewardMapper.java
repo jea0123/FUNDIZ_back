213700f8963eb38b688b0bf5220be7dd62ba83f9
jea0123 <eunae8895@gmail.com>
2025-09-11 15:40:38 +0900

    feat(reward): update/delete reward

--- src/main/java/com/example/funding/mapper/RewardMapper.java
+++ src/main/java/com/example/funding/mapper/RewardMapper.java
@@ -1,6 +1,6 @@
 package com.example.funding.mapper;
 
-import com.example.funding.dto.request.reward.RewardCreateRequestDto;
+import com.example.funding.dto.request.reward.RewardUpdateRequestDto;
 import com.example.funding.model.Reward;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
@@ -9,9 +9,13 @@ import java.util.List;
 
 @Mapper
 public interface RewardMapper {
-    List<Reward> getRewardListById(@Param("projectId") Long projectId);
+    List<Reward> getRewardList(@Param("projectId") Long projectId);
 
     List<Reward> findProjectIdsByRewardIds(@Param("ids") List<Long> rewardIds);
 
     void saveReward(Reward reward);
+
+    int updateReward(RewardUpdateRequestDto dto);
+
+    int deleteReward(Long projectId, Long rewardId);
 }

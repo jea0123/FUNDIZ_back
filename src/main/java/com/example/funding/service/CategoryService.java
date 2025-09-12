200805bf43df2ee555c0a7fa243136de6e0d5108
jea0123 <eunae8895@gmail.com>
2025-09-12 19:07:20 +0900

    feat(category): category, subcategory

--- src/main/java/com/example/funding/service/CategoryService.java
+++ src/main/java/com/example/funding/service/CategoryService.java
@@ -1,6 +1,7 @@
 package com.example.funding.service;
 
 import com.example.funding.dto.ResponseDto;
+import com.example.funding.dto.response.project.SubcategoryDto;
 import com.example.funding.model.Category;
 import org.springframework.http.ResponseEntity;
 
@@ -8,4 +9,6 @@ import java.util.List;
 
 public interface CategoryService {
     ResponseEntity<ResponseDto<List<Category>>> getAllCategories();
+
+    ResponseEntity<ResponseDto<List<SubcategoryDto>>> getAllSubcategories();
 }
--- src/main/java/com/example/funding/service/impl/CategoryServiceImpl.java
+++ src/main/java/com/example/funding/service/impl/CategoryServiceImpl.java
@@ -1,6 +1,7 @@
 package com.example.funding.service.impl;
 
 import com.example.funding.dto.ResponseDto;
+import com.example.funding.dto.response.project.SubcategoryDto;
 import com.example.funding.mapper.CategoryMapper;
 import com.example.funding.model.Category;
 import com.example.funding.service.CategoryService;
@@ -32,5 +33,11 @@ public class CategoryServiceImpl implements CategoryService {
     public ResponseEntity<ResponseDto<List<Category>>> getAllCategories() {
         List<Category> categories = categoryMapper.getAllCategories();
         return ResponseEntity.status(HttpStatus.OK).body(ResponseDto.success(200,"카테고리 조회 성공", categories));
+    }
+
+    @Override
+    public ResponseEntity<ResponseDto<List<SubcategoryDto>>> getAllSubcategories() {
+        List<SubcategoryDto> subcategories = categoryMapper.getAllSubcategories();
+        return ResponseEntity.ok(ResponseDto.success(200, "서브카테고리 조회 성공", subcategories));
     }
 }

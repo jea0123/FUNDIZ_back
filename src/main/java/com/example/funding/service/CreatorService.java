df8e13bdbddd3ae384de6d1e97cab5f50e97232a
LeeYG03 <LeeYG3673@gmail.com>
2025-10-02 18:00:08 +0900

    10_02 Working on code

--- src/main/java/com/example/funding/service/CreatorService.java
+++ src/main/java/com/example/funding/service/CreatorService.java
@@ -1,6 +1,7 @@
 package com.example.funding.service;
 
 import com.example.funding.dto.ResponseDto;
+import com.example.funding.dto.response.creator.CreatorPDetailDto;
 import com.example.funding.dto.response.creator.CreatorPListDto;
 import org.springframework.http.ResponseEntity;
 
@@ -8,4 +9,8 @@ import java.util.List;
 
 public interface CreatorService {
     ResponseEntity<ResponseDto<List<CreatorPListDto>>> getCreatorPList(Long creatorId);
+
+    ResponseEntity<ResponseDto<CreatorPDetailDto>> getCreatorDList(Long creatorId, Long projectId);
+
+    ResponseEntity<ResponseDto<CreatorPDetailDto>> getCreatorDashBoard(Long creatorId);
 }

1a1abd8aa502534b9cee970f1a18ca23163d2bff
KRMGJ <dur369667@naver.com>
2025-10-07 14:22:59 +0900

    feat(user): add creatorId parameter to CustomUserPrincipal for enhanced user identification

--- src/main/java/com/example/funding/common/CustomUserPrincipal.java
+++ src/main/java/com/example/funding/common/CustomUserPrincipal.java
@@ -9,6 +9,7 @@ import java.util.Collection;
  * <p>커스텀 유저 프린시펄</p>
  * <p>- Spring Security의 UserDetails를 구현하여 사용자 인증 정보를 담는 객체</p>
  *
+ * @param creatorId   크리에이터 ID (없으면 null)
  * @param userId      사용자 ID
  * @param email       사용자 이메일 (username 역할)
  * @param authorities 사용자 권한 목록

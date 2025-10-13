e310ad4f6e74f7a49cc8cedf0607667aaeacaa94
LeeYG03 <LeeYG3673@gmail.com>
2025-10-13 21:17:11 +0900

    10/13commit

---
+++ src/main/java/com/example/funding/dto/response/payment/BackingPagePaymentDto.java
@@ -0,0 +1,9 @@
+package com.example.funding.dto.response.payment;
+
+public class BackingPagePaymentDto {
+    private String orderId;
+    private String method;
+    private String status;
+    private Long amount;
+    private String cardCompany;
+}

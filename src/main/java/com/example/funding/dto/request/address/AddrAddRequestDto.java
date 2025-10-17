822eb3b2693784fd4c87c21d7b7e5118da9fe081
KRMGJ <dur369667@naver.com>
2025-10-17 23:47:08 +0900

    refactor: format code and organize imports in request DTOs

--- src/main/java/com/example/funding/dto/request/address/AddrAddRequestDto.java
+++ src/main/java/com/example/funding/dto/request/address/AddrAddRequestDto.java
@@ -1,21 +1,21 @@
-    package com.example.funding.dto.request.address;
+package com.example.funding.dto.request.address;
 
-    import lombok.AllArgsConstructor;
-    import lombok.Builder;
-    import lombok.Getter;
-    import lombok.Setter;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
 
-    @Getter
-    @Setter
-    @Builder
-    @AllArgsConstructor
-    public class AddrAddRequestDto {
-        private String addrName;
-        private String recipient;
-        private String postalCode;
-        private String roadAddr;
-        private String detailAddr;
-        private String recipientPhone;
-        private Character isDefault;
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+public class AddrAddRequestDto {
+    private String addrName;
+    private String recipient;
+    private String postalCode;
+    private String roadAddr;
+    private String detailAddr;
+    private String recipientPhone;
+    private Character isDefault;
 
-    }
+}

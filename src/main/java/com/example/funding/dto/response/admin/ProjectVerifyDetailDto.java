bc8929a2c3e88bcd468e655742ca88bec1d5577d
jea0123 <eunae8895@gmail.com>
2025-10-19 14:04:37 +0900

    feat(dto, sql): add fields to AdminProjectListDto/ProjectVerifyDetailDto and update queries in admin.xml

--- src/main/java/com/example/funding/dto/response/admin/ProjectVerifyDetailDto.java
+++ src/main/java/com/example/funding/dto/response/admin/ProjectVerifyDetailDto.java
@@ -12,20 +12,19 @@ import java.util.List;
 @Setter
 public class ProjectVerifyDetailDto {
     private Long projectId;
-    private Long creatorId;
     private String title;
-    private String content;
-    private String thumbnail;
     private Integer goalAmount;
     private LocalDateTime startDate;
     private LocalDateTime endDate;
+    private String content;
+    private String contentBlocks;
+    private String thumbnail;
+    private String businessDoc;
     private String projectStatus;
     private LocalDateTime requestedAt;
 
-    private Long ctgrId;
-    private String ctgrName;
-    private Long subctgrId;
     private String subctgrName;
+    private String ctgrName;
 
     private String creatorName;
     private String businessNum;
